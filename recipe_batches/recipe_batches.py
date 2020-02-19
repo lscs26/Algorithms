59e86e55df33f33bff79646c758ea3bc9c639e9a
kenna <kennaalastair@gmail.com>
2020-02-19 15:52:03 -0800

    recipe_batches.py tests passing

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,7 +3,20 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  for key in recipe:
+    if key not in ingredients or len(recipe) > len(ingredients):
+        return 0
+    else:
+        batches = [0] * len(recipe)
+        index = 0
+        # Get the number of batches per ingredient
+        for key, value in recipe.items():
+            batches[index] = ingredients[key] // recipe[key]
+            index += 1
+        return min(batches)
+
+print(recipe_batches({'milk': 100, 'butter': 50, 'flour': 5},
+                    {'milk': 138, 'butter': 52, 'flour': 51}))
 
 
 if __name__ == '__main__':
