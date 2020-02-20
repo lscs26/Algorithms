15820a42043cd3a11e55e744ef4110297f732eed
kenna <kennaalastair@gmail.com>
2020-02-20 10:32:16 -0800

    added cache to line 20

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -17,7 +17,7 @@ def eating_cookies(n, cache=None):
   else:
     if not cache:
       cache = {i: 0 for i in range(n+1)}
-    value = eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3, cache)
+    value = eating_cookies(n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
     cache[n] = value
     return value
     
