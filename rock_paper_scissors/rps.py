e164994065de5cb84544278005aaa9db088cda81
kenna <kennaalastair@gmail.com>
2020-02-20 16:01:54 -0800

    logic, test still not passing

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,9 +1,46 @@
 #!/usr/bin/python
 
 import sys
+from random import randint
+
+
+rock = ["rock"]
+paper = ["paper"]
+scissors = ["scissors"]
+rps = [rock,paper,scissors]
+available_options = ['rock', 'paper', 'scissors']
 
 def rock_paper_scissors(n):
-  pass 
+    if n==0:
+        return [[]]
+    if n==1:
+        return rps
+    return rps_helper(n,rock)+rps_helper(n,paper)+rps_helper(n,scissors)
+
+def rps_helper(n,list):
+    if n ==1:
+        return list
+    if n ==2:
+        l1=list.copy()
+        l2=list.copy()
+        l3=list.copy()
+        l1.extend(rock)
+        l2.extend(paper)
+        l3.extend(scissors)
+        n-=1
+        return [rps_helper(n,l1), rps_helper(n,l2), rps_helper(n,l3)]
+    else:
+        l1=list.copy()
+        l2=list.copy()
+        l3=list.copy()
+        l1.extend(rock)
+        l2.extend(paper)
+        l3.extend(scissors)
+        n-=1
+        return rps_helper(n,l1), rps_helper(n,l2), rps_helper(n,l3)
+        
+print(rock_paper_scissors(3))
+
 
 
 if __name__ == "__main__":
